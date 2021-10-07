3f4f03d849b700be0230fa3032fef6b1b8fadbf4
Andy Gonzalez <andy@deepdishtech.com>
2021-10-07 15:47:26 -0400

    add sample to show location inside webview

---
+++ HybridWebView.UWP/BaseUrl.cs
@@ -0,0 +1,16 @@
+﻿using HybridWebView.UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BaseUrl))]
+namespace HybridWebView.UWP {
+    public class BaseUrl : IBaseUrl {
+        public string Get() {
+            return "ms-appx-web:///";
+        }
+    }
+}
--- HybridWebView/MainPage.xaml.cs
+++ HybridWebView/MainPage.xaml.cs
@@ -11,7 +11,27 @@ namespace HybridWebView {
         public MainPage() {
             InitializeComponent();
 
-            hybridWebView.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+            // webView.Source = "https://www.google.com";
+
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.BaseUrl = DependencyService.Get<IBaseUrl>().Get();
+
+            string html = @"
+<html>
+<head>
+    <title>Xamarin Forms</title>
+</head>
+<body>
+    <script src=""http://code.jquery.com/jquery-2.1.4.min.js""></script>
+    <h1>Xamarin.Forms</h1>
+    <pre id=""location"">Location: </pre>
+    <button onclick=""javascript: $('#location').text('Location: ' + window.location.href);"">Show window.location.href</button>
+</body>
+</html>
+";
+            htmlSource.Html = html;
+
+            webView.Source = htmlSource;
         }
     }
 }
