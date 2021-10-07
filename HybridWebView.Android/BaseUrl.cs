bcc1fc6b7072eb876fc7874fbab7311f84e14376
Andy Gonzalez <andy@miratasoft.com>
2021-10-07 16:09:05 -0400

    ios + android changes

---
+++ HybridWebView.Android/BaseUrl.cs
@@ -0,0 +1,12 @@
+﻿using HybridWebView.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BaseUrl))]
+namespace HybridWebView.Droid {
+    public class BaseUrl : IBaseUrl {
+        public string Get() {
+            return "file:///android_asset/";
+        }
+    }
+}
+
---
+++ HybridWebView.iOS/BaseUrl.cs
@@ -0,0 +1,12 @@
+﻿using HybridWebView.iOS;
+using Foundation;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BaseUrl))]
+namespace HybridWebView.iOS {
+    public class BaseUrl : IBaseUrl {
+        public string Get() {
+            return NSBundle.MainBundle.BundlePath;
+        }
+    }
+}
